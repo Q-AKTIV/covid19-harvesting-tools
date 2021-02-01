bdaf980cb4e8fe954fc9a0ba4c5288aa9b793887
Eva Seidlmayer <seidlmayer@zbmed.de>
2021-02-01 16:42:50 +0000

    Upload New File

---
+++ SRU-KE_subject-covid19_date-2020.sh
@@ -0,0 +1,1 @@
+curl "http://z3950.zbmed.de:6210/livivo?version=2.0&operation=searchRetrieve&query=dc.subject=covid19%20AND%20dc.date=2020&facetLimit=0&startRecord=[1-558392:5000]&recordSchema=xml&x-username=zbmintern&maximumRecords=5000" -O "2021_ZBMED-COVID19-#1.xml" 
